1b5150ac3de761266ba1c768c35790288545e251
Gabriel Santos <gabilubastos@gmail.com>
2023-03-29 16:30:14 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const form = document.querySelector('.quiz-form')
+const finalResult = document.querySelector('.result')
+
+const correctAswers = ['B', 'B', 'B', 'B']
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+
+    let score = 0
+    const userAnswers = [
+        form.inputQuestion1.value,
+        form.inputQuestion2.value,
+        form.inputQuestion3.value,
+        form.inputQuestion4.value
+    ]
+
+    userAnswers.forEach((userAnswer, index) => {
+        if (userAnswer === correctAswers[index]){
+            score += 25
+            console.log('acertou')
+        }
+    })
+
+    scrollTo(0, 0)
+
+   
+    finalResult.classList.remove('d-none')
+
+    let counter = 0
+
+    const timer = setInterval(() => {
+        if(counter === score) {
+            clearInterval(timer)
+        }
+
+        finalResult.querySelector('span').textContent = `${counter}%`
+        counter++
+    }, 10)
+})
